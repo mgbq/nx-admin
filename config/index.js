70c01eaac955dae89e8c8da81b146ff00b9cc710
cjx <cjx@example.com>
2018-06-29 09:12:39 +0800

    add

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-permission/',
+    assetsPublicPath: '/nx-admin/',
 
     /**
      * Source Maps
